5714b7777e414b3e222f87a18e8d65d21c8b8c50
lohanson33@gmail.com <lohanson33@gmail.com>
2022-12-01 15:56:09 -0600

    Locations linked to employees

--- src/components/employees/NewEmployeeForm.js
+++ src/components/employees/NewEmployeeForm.js
@@ -5,47 +5,65 @@ import "./Employee.css"
 
 export const NewEmployeeForm = () => {
 
-    const [form, setForm] = useState({
-        name: "",
-        location: "",
-        startDate: Date,
-        rate: 0
 
+    const [newEmployee, setNewEmployee] = useState({
+        id: 0,
+        name: "",
+        email: "",
+        isStaff: true
     })
 
-    const kandyUser = localStorage.getItem("kandy_user")
-    const kandyUserObject = JSON.parse(kandyUser)
-
     const navigate = useNavigate()
+
 
     const handleSaveButtonClick = (event) => {
         event.preventDefault()
 
-        const newEmployeeForm = {
-            userId: kandyUserObject.id,
-            name: kandyUserObject.name,
-            location: form.location,
-            date: "",
-            rate: kandyUserObject.rate
+        const newUser = {
+            id: newEmployee.id,
+            name: newEmployee.name,
+            email: newEmployee.email,
+            isStaff: newEmployee.isStaff
         }
 
-        return fetch("http://localhost:8088/employees", {
+        const newEmployee = {
+            userId: newEmployee.userId,
+            location: newEmployee.locationId,
+            startDate: newEmployee.startDate,
+            payRate: newEmployee.payRate,
+
+        }
+
+        return fetch("http://localhost:8088/users", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(newEmployeeForm)
+            body: JSON.stringify(newUser)
+        })
+            .then((response) => response.json)
+            
+
+
+
+         fetch("http://localhost:8088/employees", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newEmployee)
         })
 
             .then((response) => response.json)
-            .then(() => {
-                navigate("/employee")
-            })
+            .then(
+            )
     }
+
 
     return (
         <form className="newHireForm">
             <h2 className="newHireForm_header">New Hire Form</h2>
+
             <fieldset>
                 <div className="form-group">
                     <label htmlFor="name">Name: </label>
@@ -53,18 +71,92 @@ export const NewEmployeeForm = () => {
                         required autoFocus
                         type="text"
                         className="form-control"
-                        value={form.name}
+                        value={newEmployee.name}
+                        onChange={
+                            (event) => {
+                                const copy = { ...newEmployee }
+                                copy.name = event.target.checked
+                                setNewEmployee(copy)
+                            }
+                        }
                     />
                 </div>
             </fieldset>
+
             <fieldset>
                 <div className="form-group">
-                    <label>Location</label>
-                    <input 
-                    type="radio"/> 
+                    <label htmlFor="email">Email: </label>
+                    <input
+                        required autoFocus
+                        type="text"
+                        className="form-control"
+                        value={newEmployee.email}
+                        onChange={
+                            (event) => {
+                                const copy = { ...newEmployee }
+                                copy.email = event.target.value
+                                setNewEmployee(copy)
+                            }
+                        }
+                    />
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <div className="form-group">
+                    <input type="checkbox"
+                        value={newEmployee.isStaff}
+                        onChange={
+                            (event) => {
+                                const copy = { ...newEmployee }
+                                copy.isStaff = event.target.checked
+                                setNewEmployee(copy)
+                            }
+                        } />
+                    <label htmlFor="staff">Staff</label>
 
                 </div>
             </fieldset>
+
+            {/* <fieldset>
+                <div className="form-group">
+                    <label>Location: </label>
+                    <select id="locations">
+                        <option value="0">
+                            Choose a location...
+                        </option>
+
+                    </select>
+
+                </div>
+            </fieldset> */}
+
+            {/* <fieldset>
+                <div className="form-group">
+                    <label htmlFor="startDate">Start Date: </label>
+                    <input
+                        required autoFocus
+                        type="date"
+                        className="form-control"
+                        name="date"
+                    />
+                </div>
+            </fieldset>
+
+            <fieldset>
+                <div className="form-group">
+                    <label htmlFor="rate">Pay Rate per Hour:</label>
+                    <input type="number"
+                        className="form-control"
+                    />
+                </div>
+            </fieldset> */}
+
+            <button
+                onClick={(clickEvent) => { handleSaveButtonClick(clickEvent) }}
+                className="btn btn-primary">
+                Save New Employee
+            </button>
         </form>
     )
 
